190409cfd6c2090bc80a889edb93d3279c45bb38
Giorgos-Vitsos <csd5369@csd.uoc.gr>
2026-06-29 13:15:04 +0300

    Fix input mapping and refactor character movement
    Fixed swapped Left/Right input action bindings. Refactored CharController to use Lane enum, split movement logic into separate methods (CalculateLaneVelocity, ApplyMovement), and improved lateral velocity calculation. Updated lane distance to 4.5 and track width to 1.6. Added Track prefab.

--- Assets/Scripts/CharController.cs
+++ Assets/Scripts/CharController.cs
@@ -7,10 +7,10 @@ using UnityEngine.InputSystem;
 public class CharController : MonoBehaviour
 {
     [Header("Movement")]
-    [SerializeField] private float runningSpeed=5f;
-    [SerializeField] private float snappingForce=5f;
-    [SerializeField] private float laneDistance=3f;
-    
+    [SerializeField] private float runningSpeed = 5f;
+    [SerializeField] private float snappingForce = 5f;
+    [SerializeField] private float laneDistance = 4.5f;
+
 
     [Header("References")]
     [SerializeField] private InputActionReference moveLeftAction;
@@ -18,49 +18,79 @@ public class CharController : MonoBehaviour
 
 
     private CharacterController _controller;
-    private int _currentLane=0; //-1,0,1=left,middle,right
+    private Lane _currentLane = Lane.middle;
+    private float _lateralVelocity;
 
-
+    private enum Lane
+    {
+        left = -1,
+        middle = 0,
+        right = 1
+    }
 
     void Awake()
     {
-        _controller=GetComponent<CharacterController>();
+        _controller = GetComponent<CharacterController>();
     }
 
     void OnEnable()
     {
-        moveLeftAction.action.performed+=LeftAction;
-        moveRightAction.action.performed+=RightAction;
-        moveLeftAction.action.canceled+=LeftAction;
-        moveRightAction.action.canceled+=RightAction;
+        moveLeftAction.action.performed += LeftAction;
+        moveRightAction.action.performed += RightAction;
+
     }
 
     void OnDisable()
     {
-        moveLeftAction.action.performed-=LeftAction;
-        moveRightAction.action.performed-=RightAction;
-        moveLeftAction.action.canceled-=LeftAction;
-        moveRightAction.action.canceled-=RightAction;
+        moveLeftAction.action.performed -= LeftAction;
+        moveRightAction.action.performed -= RightAction;
+
     }
 
     private void RightAction(InputAction.CallbackContext context)
     {
-        if (_currentLane > -1)
+        switch (_currentLane)
         {
-            _currentLane--;
+            case Lane.left:
+                _currentLane = Lane.middle;
+                break;
+            case Lane.middle:
+                _currentLane=Lane.right;
+                break;
         }
     }
 
     private void LeftAction(InputAction.CallbackContext context)
     {
-        if (_currentLane < 1)
+        switch (_currentLane)
         {
-            _currentLane++;
+            case Lane.right:
+                _currentLane = Lane.middle;
+                break;
+            case Lane.middle:
+                _currentLane=Lane.left;
+                break;
         }
     }
 
     void Update()
     {
+
         Debug.Log(_currentLane);
+        CalculateLaneVelocity();
+        ApplyMovement();
+    }
+
+    private void CalculateLaneVelocity()
+    {
+        float targetXPosition = (int)_currentLane * laneDistance;
+        float xDifference = targetXPosition - transform.position.x;
+        _lateralVelocity = xDifference * snappingForce;
+    }
+
+    private void ApplyMovement()
+    {
+        Vector3 moveVector = new Vector3(_lateralVelocity, 0f, runningSpeed);
+        _controller.Move(moveVector * Time.unscaledDeltaTime);
     }
 }
